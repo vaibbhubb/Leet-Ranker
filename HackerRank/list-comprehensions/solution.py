09c92b167be933e4133e85c3b8cf2ce332fc59c7
vaibbhubb <vaibbhubb@gmail.com>
2026-07-08 22:04:31 +0530

    HackerRank: list-comprehensions

---
+++ HackerRank/list-comprehensions/solution.py
@@ -0,0 +1,1 @@
+if __name__ == '__main__':    x = int(input())    y = int(input())    z = int(input())    n = int(input())    ar1 = list()    for i in range(x+1):        for j in range(y+1):            for k in range(z+1):                if i+j+k!=n:                     ar1.append([i,j,k])    print(ar1)
