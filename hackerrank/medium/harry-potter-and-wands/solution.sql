1dcb7573382e7175ce21c110d6b29f3946ec5f8c
Vaibhav Gupta <vaibbhubb@gmail.com>
2026-09-21 23:12:24 +0530

    ✅ Ollivander's Inventory | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-21

--- hackerrank/medium/harry-potter-and-wands/solution.sql
+++ hackerrank/medium/harry-potter-and-wands/solution.sql
@@ -1,12 +1,19 @@
 /*
 Enter your query here.
 */
-SELECT h.hacker_id, h.name
-FROM Submissions s
-JOIN Challenges c ON s.challenge_id = c.challenge_id
-JOIN Difficulty d ON c.difficulty_level = d.difficulty_level
-JOIN Hackers h ON s.hacker_id = h.hacker_id
-WHERE s.score = d.score
-GROUP BY h.hacker_id, h.name
-HAVING COUNT(s.submission_id) > 1
-ORDER BY COUNT(s.submission_id) DESC, h.hacker_id ASC
+WITH cte AS (
+    SELECT W.id, P.age, W.coins_needed, W.power
+    FROM Wands AS W
+    INNER JOIN Wands_Property AS P
+        ON W.code = P.code
+    WHERE P.is_evil = 0
+)
+SELECT c.id, c.age, c.coins_needed, c.power
+FROM cte AS c
+WHERE c.coins_needed = (
+    SELECT MIN(c2.coins_needed)
+    FROM cte AS c2
+    WHERE c2.power = c.power
+      AND c2.age = c.age
+)
+ORDER BY c.power DESC, c.age DESC;
