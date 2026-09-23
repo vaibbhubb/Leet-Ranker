8f4f8e4e8ade92f91f6ef638ae7c0116b38eb3ad
Vaibhav Gupta <vaibbhubb@gmail.com>
2026-09-23 15:40:06 +0530

    ✅ Draw The Triangle 2 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/draw-the-triangle-2/solution.sql
+++ hackerrank/medium/draw-the-triangle-2/solution.sql
@@ -1,7 +1,7 @@
 /*
 Enter your query here.
 */
-SET @row := 21;
-SELECT REPEAT('* ', @row := @row - 1) 
+SET @row := 0;
+SELECT REPEAT('* ', @row := @row + 1) 
 FROM information_schema.tables 
 LIMIT 20;
